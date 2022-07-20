ee215332bfbc90c5943041d63f600547031b9a48
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-07-19 21:29:11 -0300

    aula 806: Login Routes-Completa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Api from "./api/Api";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
-      <h1>
-        <Api />
-      </h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
