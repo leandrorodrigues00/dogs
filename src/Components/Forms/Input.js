6b7b076a53b1c5235f97cbfe29837ba7dcb15e59
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-07-26 21:20:05 -0300

    userContext concluido !

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name, value, onChange }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -14,8 +14,9 @@ const Input = ({ label, type, name, value, onChange }) => {
         type={type}
         onChange={onChange}
         value={value}
+        onBlur={onBlur}
       />
-      <p className={styles.error}>Error</p>
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,28 +3,20 @@ import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
-  const username = useForm("email");
+  const username = useForm();
   const password = useForm();
-  console.log(password.value);
 
-  function handleSubmit(event) {
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(json);
-      });
+
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
   }
   return (
     <section>
