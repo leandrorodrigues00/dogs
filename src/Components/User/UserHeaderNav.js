b749cb74d9a1c55a876bd9c2e8555e589fa33ad2
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-08-03 21:47:08 -0300

    Inicio Menu mobile

---
+++ src/Components/User/UserHeaderNav.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
+import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
+import { ReactComponent as Adicionar } from "../../Assets/adicionar.svg";
+import { ReactComponent as Sair } from "../../Assets/sair.svg";
+
+import styles from "./UserHeaderNav.module.css";
+import useMedia from "../../Hooks/useMedia";
+
+const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia("(max-width: 40rem)");
+
+  return (
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end>
+        <MinhasFotos />
+        {mobile && "Minhas Fotos"}
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        {mobile && "Estatísticas"}
+      </NavLink>
+      <NavLink to="/conta/postar">
+        {mobile && "Adicionar Foto"}
+        <Adicionar />
+      </NavLink>
+      <button onClick={userLogout}>
+        <Sair />
+        {mobile && "Sair"}
+      </button>
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
